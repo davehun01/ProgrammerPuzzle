b1951f4c9e95c98470fcbbffbcd43d3797afadc4
Kiss David <deco1996@mailbox.hu>
2018-07-24 01:53:24 +0200

    Added files, database created.

--- app/src/main/java/com/example/dave/programmerpuzzle/Application.java
+++ app/src/main/java/com/example/dave/programmerpuzzle/Application.java
@@ -1,4 +1,24 @@
 package com.example.dave.programmerpuzzle;
 
-public class Application {
+import org.greenrobot.greendao.database.Database;
+import org.greenrobot.greendao.*;
+
+public class Application extends android.app.Application {
+    private DaoSession mDaoSession;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mDaoSession = new DaoMaster(
+                new DaoMaster.DevOpenHelper(this, "ProgrammerPuzzle.db").getWritableDb()).newSession();
+
+        // USER CREATION FOR DEMO PURPOSE
+        if(mDaoSession.getPuzzleDao().loadAll().size() == 0){
+            mDaoSession.getPuzzleDao().insert(new Puzzle(1L,"test description", "test code"));
+        }
+    }
+
+    public DaoSession getDaoSession() {
+        return mDaoSession;
+    }
 }
