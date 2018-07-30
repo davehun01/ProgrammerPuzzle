b9464725983284b241637068bf37499e049f61b1
Kiss David <deco1996@mailbox.hu>
2018-07-30 21:01:57 +0200

    Created DaoManager and DataCache.

---
+++ app/src/main/java/com/example/dave/programmerpuzzle/DataCache.java
@@ -0,0 +1,42 @@
+package com.example.dave.programmerpuzzle;
+
+import java.util.List;
+
+public class DataCache {
+
+    private DaoManager daoManager;
+
+    private List<Puzzle> puzzleList;
+
+    public DataCache(DaoManager daoManager) {
+        this.daoManager = daoManager;
+        puzzleList = daoManager.getAllPuzzles();
+    }
+
+    public List<Puzzle> getPuzzleList() {
+        return puzzleList;
+    }
+
+    public void createPuzzle(Puzzle puzzle) {
+        Long id = daoManager.insertPuzzle(puzzle);
+        puzzle.setId(id);
+        puzzleList.add(puzzle);
+    }
+
+    public void deletePuzzle(Puzzle puzzle) {
+        daoManager.deletePuzzle(puzzle);
+        puzzleList.remove(puzzle);
+    }
+
+    public void updatePuzzle(Puzzle puzzle) {
+        daoManager.updatePuzzle(puzzle);
+        for (int i = 0; i < puzzleList.size(); i++) {
+            if (puzzleList.get(i).getId().equals(puzzle.getId())) {
+                puzzleList.set(i, puzzle);
+                break;
+            }
+        }
+    }
+
+
+}
