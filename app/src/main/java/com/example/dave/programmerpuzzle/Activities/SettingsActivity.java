6b8d7a24a09ac377712a99a16b4c00d4b5cca1fb
Kiss David <deco1996@mailbox.hu>
2018-08-10 22:09:49 +0200

    SettingsActivity design

--- app/src/main/java/com/example/dave/programmerpuzzle/Activities/SettingsActivity.java
+++ app/src/main/java/com/example/dave/programmerpuzzle/Activities/SettingsActivity.java
@@ -1,5 +1,7 @@
 package com.example.dave.programmerpuzzle.Activities;
 
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,15 +9,25 @@ import com.example.dave.programmerpuzzle.R;
 
 import butterknife.ButterKnife;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
 
         ButterKnife.bind(this);
 
         getWindow().setStatusBarColor(getResources().getColor(R.color.colorDarkBlue));
+
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new SettingsFragment()).commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preference);
+        }
     }
 }
