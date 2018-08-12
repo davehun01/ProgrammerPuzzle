87b2b124684b325091679f99e181aa02274edaff
Kiss David <deco1996@mailbox.hu>
2018-08-12 20:17:54 +0200

    Saving and retrieving value from settings.

--- app/src/main/java/com/example/dave/programmerpuzzle/Activities/SettingsActivity.java
+++ app/src/main/java/com/example/dave/programmerpuzzle/Activities/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.example.dave.programmerpuzzle.Activities;
 
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -28,6 +31,24 @@ public class SettingsActivity extends PreferenceActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preference);
+            bindSummaryValue(findPreference("key_name"));
         }
+    }
+
+    private static Preference.OnPreferenceChangeListener changeListener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            String stringValue = newValue.toString();
+            if (preference instanceof EditTextPreference) {
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+    };
+
+    private static void bindSummaryValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(changeListener);
+        changeListener.onPreferenceChange(preference,
+                PreferenceManager.getDefaultSharedPreferences(preference.getContext()).getString(preference.getKey(),""));
     }
 }
